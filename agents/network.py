ab4da6b1c41a5cfb7f208d95222fa324c57853c6
Victor Oliveira <vt.victoroliveira@gmail.com>
2019-11-10 13:20:11 -0300

    include sigmoid and increase n_hidden default

--- agents/network.py
+++ agents/network.py
@@ -5,8 +5,14 @@ from keras.layers import Dense, Activation
 
 class NeuralNetwork(object):
 
-    def __init__(self, input_shape=20, output_shape=5):
+    def __init__(self, 
+                input_shape=20, 
+                n_hidden=10,
+                output_shape=5):
+
+
         self.input_shape = input_shape
+        self._n_hidden = n_hidden
         self.output_shape = output_shape
 
         self._build_model()
@@ -15,10 +21,10 @@ class NeuralNetwork(object):
     def _build_model(self):
 
         model = Sequential()
-        model.add(Dense(units=4, input_dim=self.input_shape))
+        model.add(Dense(units=self._n_hidden, input_dim=self.input_shape))
         model.add(Activation("relu"))
         model.add(Dense(units=self.output_shape))
-        model.add(Activation("softmax"))
+        model.add(Activation("sigmoid"))
 
         self._model = model
 
@@ -28,6 +34,7 @@ class NeuralNetwork(object):
     def return_action(self, inputs):
         inputs = np.array([inputs])
         probabilities = np.round(self._model.predict(inputs))[0]
+
         left = probabilities[0]
         right = probabilities[1]
         jump = probabilities[2]
@@ -36,7 +43,11 @@ class NeuralNetwork(object):
 
         return [left, right, jump, shoot, release]
 
+    def load_model(self, saved_model):
+        self._model.load_model(saved_model)
+
     def control(self, params, cont=None):
+        params = (params-min(params))/float((max(params)-min(params)))
         action1, action2, action3, action4, action5 = self.return_action(params)
 
         return [action1, action2, action3, action4, action5]
